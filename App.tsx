1127f3d85f8a0ffc33c07cabd34ed9bb1588c56a
nexoratexdigital <nexoratex.digital@gmail.com>
2026-02-05 01:59:43 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -45,7 +45,7 @@ const Navbar: React.FC<{ isOpen: boolean; setIsOpen: (val: boolean) => void }> =
       }`}>
         <div className="max-w-7xl mx-auto px-6 md:px-12 flex items-center justify-between">
           <a href="#" className="uppercase z-[110] text-xl font-semibold text-white tracking-tighter mix-blend-difference whitespace-nowrap">
-            DEJA VU
+            DEJA VU.
           </a>
           
           {/* Desktop Links */}
